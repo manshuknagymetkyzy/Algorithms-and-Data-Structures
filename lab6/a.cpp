997631558090539b87a861252c591f2c1d87acf2
Manshuk Nagymetkyzy <78359971+manshuknagymetkyzy@users.noreply.github.com>
2021-11-16 20:06:14 +0600

    Update a.cpp

--- lab6/a.cpp
+++ lab6/a.cpp
@@ -5,7 +5,7 @@ int main(){
     cin >> s >> t;
     int n = s.size();
     vector<long long> p_pow(n);
-    long long p = 5;
+    int p = 31;
     p_pow[0] = 1;
     for(int i = 1; i < n; i++){
         p_pow[i] = p_pow[i - 1] * p;
