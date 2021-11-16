939bd7a65340c22ff1eb20ba1ee0e8329260f8c4
Manshuk Nagymetkyzy <78359971+manshuknagymetkyzy@users.noreply.github.com>
2021-11-16 19:36:52 +0600

    Lab 6 A added

---
+++ lab6/a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s, t;
+    cin >> s >> t;
+    int n = s.size();
+    vector<long long> p_pow(n);
+    long long p = 5;
+    p_pow[0] = 1;
+    for(int i = 1; i < n; i++){
+        p_pow[i] = p_pow[i - 1] * p;
+    }
+    long long ht = 0;
+    for(int i = 0; i < t.size(); i++){
+        ht += (t[i] - 96) * p_pow[i];
+    }
+    vector<long long> hs(n);
+    for(int i = 0; i < n; i++){
+        hs[i] = (s[i] - 96) * p_pow[i];
+        if(i > 0) hs[i] += hs[i - 1];
+    }
+    for(int i = 0; i + t.size() - 1 < n; i++){
+        long long cur = hs[i + t.size() - 1];
+        if(i > 0) cur -= hs[i - 1];
+        if(ht * p_pow[i] == cur){
+            cout << i << " ";
+        }
+    }
+}
