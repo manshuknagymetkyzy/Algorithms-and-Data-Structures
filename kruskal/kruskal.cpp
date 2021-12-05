c4677d64d28822a57a5bf7e1b3c62a093176acb1
Manshuk Nagymetkyzy <78359971+manshuknagymetkyzy@users.noreply.github.com>
2021-12-05 09:03:02 +0600

    Add files via upload

---
+++ kruskal/kruskal.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+using namespace std;
+int n = 10;
+
+vector<int> p (105);
+
+int dsu_get (int v) {
+	if (v == p[v]) return v;
+	return p[v] = dsu_get(p[v]);
+}
+
+void dsu_unite (int a, int b) {
+	a = dsu_get (a); //a = 1
+	b = dsu_get (b); //b = 2
+	if (rand() & 1) 
+		swap (a, b); //a = 1, b = 2
+	if (a != b)
+		p[a] = b;
+}
+
+
+int main(){
+	freopen("input.in", "r", stdin);
+	int n, m;
+	cin >> n >> m;
+	vector < pair < int, pair<int,int> > > g; 
+	int cost = 0;
+	vector < pair<int,int> > res;
+	for (int i = 0; i < m; i ++){
+		int u, v, w;
+		cin >> u >> v >> w;
+		// cout << u << v << w << endl;
+		g.push_back({w, {u,v}});
+		g.push_back({w, {v,u}});
+	}
+  	sort (g.begin(), g.end());
+  	cout << g.size() << endl;
+	for (int i = 0; i < g.size(); i ++){
+		cout << g[i].first << "  (" << g[i].second.first << ", " << g[i].second.second << ")" << endl;
+	}
+	p.resize (n);
+	for (int i=0; i<n; ++i){
+		p[i] = i;
+	}
+	for (int i=0; i<m; ++i) {
+		int a = g[i].second.first;
+		int b = g[i].second.second;
+		int l = g[i].first;
+
+		if (dsu_get(a) != dsu_get(b)) {
+			cost += l;
+			res.push_back (g[i].second);
+			dsu_unite (a, b);
+		}
+	}
+	cout << endl;
+	cout << cost << endl;
+	for (int i = 0; i < res.size(); i ++){
+		cout << res[i].first << " " << res[i].second << endl;
+	}
+}
+
